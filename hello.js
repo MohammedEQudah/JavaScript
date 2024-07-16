b593d4d71da1f4a572208e36bd18109b69f7ed16
MohammedEQudah <mh.qud2551996@gmail.com>
2024-07-16 21:25:34 +0300

    javascript task

---
+++ hello.js
@@ -0,0 +1,5 @@
+let username = prompt("Enter you Name ");
+alert('welcome ' + username + ' enjoy');
+/* try to name the variable (name) but the vscode still give me
+ this is const (i dont know (name) is a Reserved word in js and take from me 30 min to know whats the problem ) 
+ These problems make me love programming more*/
